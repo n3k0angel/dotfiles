6e70b85b3b0ef7293e27ea0a13b43387d65d4555
n3k0angel <nekoangel@incel.email>
2024-04-29 00:37:14 +0300

    updated readme and made a install script (not tested :P)

--- README.md
+++ README.md
@@ -1,30 +1,41 @@
-# dotfiles
-groups to make stuff easier
+# Install script
+```
+cd && git clone https://github.com/n3k0angel/dotfiles && cd dotfiles && chmod +x install.sh && ./install.sh
+```
+
+# Manual install
+Groups to make stuff easier
 ```sh
 $ sudo usermod -aG network,floppy,scanner,power,video,storage,optical,kvm,input,disk,audio $USER
 ```
 
+Needed packages
 ```sh
 $ sudo pacman -S --needed git base-devel man pipewire pipewire-pulse wireplumber neovim mpv hdparm bat powertop keepassxc feh fzf btop udisks2 dunst acpi brightnessctl slock pcmanfm-gtk3
 ```
+
+Paru
 ```sh
 $ sudo pacman -S --needed base-devel git && git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si && paru --gendb && cd .. && sudo rm -rv paru-bin
 ```
+
+AUR stuff
 ```sh
-paru -S lf bashmount ctpv newsraft numlockx librewolf-bin brave-bin ungoogled-chromium-bin compfy invidtui-bin gpg-tui
+$ paru -S lf bashmount ctpv newsraft numlockx librewolf-bin brave-bin ungoogled-chromium-bin compfy invidtui-bin gpg-tui
 ```
-bash prompt
+
+Bash prompt
 ```sh
 PS1="\[\e[38;5;153m\][\u\[\e[38;5;153m\]@\[\e[38;5;153m\]\h \[\e[38;5;214m\]\w\[\e[38;5;153m\]]\[\033[0m\]$ "
 ```
 
-thicker fonts:
+Thicker fonts:
 ```sh
 /etc/environment
 FREETYPE_PROPERTIES="cff:no-stem-darkening=0 autofitter:no-stem-darkening=0"
 ```
 
-make sure you've logged out and back in at least once 
+Fonts (make sure you've logged out and back in at least once)
 ```sh
 $ paru -S ttf-jetbrains-mono-nerd ttf-ms-win11-auto adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts noto-fonts-emoji && paru -Rns gnu-free-fonts
 ```
---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/sh
+set -e
+
+if [ $EUID = 0 ]; then
+    echo "Do not run as root!"
+    exit
+fi
+
+GITDIR="/home/$USER/dotfiles"
+CONFDIR="/home/$USER/.config/"
+SSHDIR="/home/$USER/.ssh/"
+
+mkdir -p $CONFDIR
+mkdir -p $SSHDIR
+
+# Add $USER to needed groups
+sudo usermod -aG network,floppy,scanner,power,video,storage,optical,kvm,input,disk,audio $USER
+
+# Install stuff
+sudo pacman -S --needed --noconfirm git base-devel man pipewire pipewire-pulse wireplumber neovim mpv hdparm bat powertop keepassxc feh fzf btop udisks2 dunst acpi brightnessctl slock pcmanfm-gtk3
+
+# Install paru
+cd /home/$USER
+sudo pacman -S --needed --noconfirm base-devel git 
+git clone https://aur.archlinux.org/paru-bin.git 
+cd paru-bin 
+makepkg -si 
+paru --gendb 
+cd .. 
+sudo rm -rv paru-bin
+
+cd $GITDIR
+
+# Install more stuff
+paru -S --needed --noconfirm lf bashmount ctpv newsraft numlockx librewolf-bin brave-bin ungoogled-chromium-bin compfy invidtui-bin gpg-tui
+
+# Bash prompt
+sed '/PS1/d' /home/$USER/.bashrc > /home/$USER/bashrc-temp
+mv /home/$USER/bashrc-temp /home/$USER/.bashrc
+echo "EDITOR=nvim" >> /home/$USER/.bashrc
+echo 'alias ls="ls -lah --color=always"' >> /home/$USER/.bashrc
+echo 'alias bm="bashmount"' >> /home/$USER/.bashrc
+
+# Thicker fonts
+sudo chmod o+w /etc/environment
+echo 'FREETYPE_PROPERTIES="cff:no-stem-darkening=0 autofitter:no-stem-darkening=0"' >> /etc/environment
+sudo chmod o-w /etc/environment
+
+# Install configs
+cp -r .config/* $CONFDIR/
+cp -r .ssh/* $SSHDIR/
+
+# Create a script to be run after logging out and back in
+echo '#!/usr/bin/sh' > /home/$USER/run-me.sh
+echo 'paru -S --noconfirm ttf-jetbrains-mono-nerd ttf-ms-win11-auto adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts noto-fonts-emoji && paru -Rns gnu-free-fonts' >> /home/$USER/run-me.sh
+echo 'echo "Done!!!"' >> /home/$USER/run-me.sh
+echo 'rm /home/$USER/run-me.sh' >> /home/$USER/run-me.sh
+
+chmod +x /home/$USER/run-me.sh
+echo "Log out and back in, then run 'run-me.sh'"
+
+exit
